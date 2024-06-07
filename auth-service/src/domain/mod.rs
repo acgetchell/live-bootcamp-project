db3ef60b90ad46fe1189820eed0b2e3c52adda99
Adam Getchell <acgetchell@ucdavis.edu>
2024-06-07 07:41:49 -0700

    feat: Most of Sprint 3

--- auth-service/src/domain/mod.rs
+++ auth-service/src/domain/mod.rs
@@ -1,4 +1,6 @@
+mod error;
 mod user;
 
 // re-export from sub-modules
+pub use error::*;
 pub use user::*;
--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -3,25 +3,39 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
-use crate::{app_state::AppState, domain::User};
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, User},
+};
 
 pub async fn signup(
     State(state): State<AppState>,
     Json(request): Json<SignupRequest>,
-) -> impl IntoResponse {
-    // Create a new `User` instance using data in the `request` variable
-    let user = User::new(request.email, request.password, request.requires_2fa);
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email = request.email;
+    let password = request.password;
+
+    // TODO: early return AuthAPIError::InvalidCredentials if:
+    // - the email is empty or does not contain '@'
+    // - the password is less than 8 characters
+
+    let user = User::new(email, password, request.requires_2fa);
 
     let mut user_store = state.user_store.write().await;
 
-    // Add the user to the user store
+    // TODO: early return AuthAPIError::UserAlreadyExists if email exists in user_store
+    // if user_store.get_user(&user.email).await.is_ok() {
+    //     return Err(AuthAPIError::UserAlreadyExists);
+    // }
+
+    // TODO: instead of using unwrap, early return AuthAPIError::UnexpectedError if the user could not be added to the user store
     user_store.add_user(user).unwrap();
 
     let response = Json(SignupResponse {
         message: "User created successfully!".to_string(),
     });
 
-    (StatusCode::CREATED, response)
+    Ok((StatusCode::CREATED, response))
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -31,8 +45,8 @@ pub struct SignupResponse {
 
 #[derive(Deserialize)]
 pub struct SignupRequest {
-    email: String,
-    password: String,
+    pub email: String,
+    pub password: String,
     #[serde(rename = "requires2FA")]
-    requires_2fa: bool,
+    pub requires_2fa: bool,
 }
