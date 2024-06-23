cd329854249264060a9823359ea70c31a0ae55ea
Adam Getchell <acgetchell@ucdavis.edu>
2024-06-22 20:28:14 -0700

    Sprint 6 Task 4 Step 2

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -1,4 +1,5 @@
-use std::sync::Arc;
+#![allow(dead_code)]
+use std::{str::FromStr, sync::Arc};
 
 use auth_service::{
     app_state::{AppState, BannedTokenStoreType, TwoFACodeStoreType},
@@ -11,7 +12,10 @@ use auth_service::{
     Application,
 };
 use reqwest::cookie::Jar;
-use sqlx::{postgres::PgPoolOptions, Executor, PgPool};
+use sqlx::{
+    postgres::{PgConnectOptions, PgPoolOptions},
+    Connection, Executor, PgConnection, PgPool,
+};
 use tokio::sync::RwLock;
 use uuid::Uuid;
 
@@ -21,11 +25,14 @@ pub struct TestApp {
     pub banned_token_store: BannedTokenStoreType,
     pub two_fa_code_store: TwoFACodeStoreType,
     pub http_client: reqwest::Client,
+    pub db_name: String,
+    pub clean_up_called: bool,
 }
 
 impl TestApp {
     pub async fn new() -> Self {
-        let pg_pool = configure_postgresql().await;
+        let db_name = Uuid::new_v4().to_string();
+        let pg_pool = configure_postgresql(&db_name).await;
         let user_store = Arc::new(RwLock::new(PostgresUserStore::new(pg_pool)));
         let banned_token_store = Arc::new(RwLock::new(HashSetBannedTokenStore::default()));
         let two_fa_code_store = Arc::new(RwLock::new(HashMapTwoFACodeStore::default()));
@@ -58,6 +65,8 @@ impl TestApp {
             banned_token_store,
             two_fa_code_store,
             http_client,
+            db_name,
+            clean_up_called: false,
         }
     }
 
@@ -124,17 +133,31 @@ impl TestApp {
             .await
             .expect("Failed to execute request")
     }
+
+    pub async fn clean_up(&mut self) {
+        if self.clean_up_called {
+            return;
+        }
+
+        delete_database(&self.db_name).await;
+        self.clean_up_called = true;
+    }
 }
+
+// impl Drop for TestApp {
+//     fn drop(&mut self) {
+//         if !self.clean_up_called {
+//             panic!("TestApp::clean_up was not called before dropping TestApp")
+//         }
+//     }
+// }
 
 pub fn get_random_email() -> String {
     format!("{}@example.com", Uuid::new_v4())
 }
 
-async fn configure_postgresql() -> PgPool {
+async fn configure_postgresql(db_name: &str) -> PgPool {
     let postgres_conn_url = DATABASE_URL.to_owned();
-
-    // We are creating a new database for each test case, so need unique names
-    let db_name = Uuid::new_v4().to_string();
 
     configure_database(&postgres_conn_url, &db_name).await;
 
@@ -172,4 +195,36 @@ async fn configure_database(db_conn_string: &str, db_name: &str) {
         .run(&connection)
         .await
         .expect("Failed to migrate the database!");
+}
+
+async fn delete_database(db_name: &str) {
+    let postgres_conn_url: String = DATABASE_URL.to_owned();
+
+    let connection_options = PgConnectOptions::from_str(&postgres_conn_url)
+        .expect("Failed to parse PostgreSQL connection string!");
+
+    let mut connection = PgConnection::connect_with(&connection_options)
+        .await
+        .expect("Failed to connect to PostgreSQL!");
+
+    // Kill any active connections to the database
+    connection
+        .execute(
+            format!(
+                r#"SELECT pg_terminate_backend(pg_stat_activity.pid)
+            FROM pg_stat_activity
+            WHERE pg_stat_activity.datname = '{}'
+            AND pid <> pg_backend_pid();"#,
+                db_name
+            )
+            .as_str(),
+        )
+        .await
+        .expect("Failed to kill active connections!");
+
+    // Drop the database
+    connection
+        .execute(format!(r#"DROP DATABASE "{}";"#, db_name).as_str())
+        .await
+        .expect("Failed to drop database!");
 }
